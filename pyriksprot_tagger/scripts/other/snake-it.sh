319cf0097abdf8befa64bb609d7f7f27028a6a98
roger-mahler <roger.mahler@hotmail.com>
2025-05-21 07:22:34 +0200

    chore: moved snakemake files to purgatory

--- pyriksprot_tagger/scripts/other/snake-it.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source .env
-VERSION=v1.y.z
-export OMP_NUM_THREADS=16
-for ((year=1920;year<=2020;year++)); do
-	echo $year
-	nohup make annotate YEAR=$year PROCESSES_COUNT=1 &>> tag.${VERSION}.log
-done
